197100889fc1c7b03e9e0b6965fd553e4a137aa2
Freddie Poser <freddie.poser@gmail.com>
2016-05-20 11:23:38 +0200

    Added Error

---
+++ BlueError/BlueError.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BluePost;
+
+function customError($type = "SE", $subType = "GE", $section = null, $message = "An error occurred") {
+    $errorCode = $type . "/" . $subType . ($section != null ? "/" . $section : "");
+    return array (
+        "error" => true,
+        "code"  => array (
+            "type" => $type,
+            "subtype" => $subType,
+            "section" => $section,
+            "code" => $errorCode
+        ),
+        "message" => $message
+    );
+}
+
