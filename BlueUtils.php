15b9c4e3aa8899763fee0d0863a72ca9ffbb03f0
Freddie Poser <freddie.poser@gmail.com>
2016-06-03 10:34:07 +0200

    Change directory structure of BlueAjax

--- BlueUtils.php
+++ BlueUtils.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once ("BlueError/BlueError.php");
-require_once ("BlueAjax/src/BlueAjax.php");
+require_once (__DIR__ . "/BlueError/BlueError.php");
+require_once (__DIR__ . "/BlueAjax/php/BlueAjax.php");
 
