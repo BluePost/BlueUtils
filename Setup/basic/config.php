744122dd0d890889b86b1f33ddc05d00499b2aef
Bluepost <freddie.poser@gmail.com>
2016-08-03 12:29:38 +0100

    Add Jame's comment

--- Setup/basic/config.php
+++ Setup/basic/config.php
@@ -3,7 +3,9 @@ $CONFIG = Array("NAME" => "This is the name of the project");
 
 $BUNDLER_CONFIG_ARRAY = Array (
     "files" => Array (
-	    //"index" => "main.twig",
+		//Hey there maintainer. You may believe it to be a good idea in adding a "users/happy/havefun" page - it works with bundler after all! but you are wrong. Like really very wrong. You couldn't be more wrong in fact. All paths will be broken. Very broken. Such is the lovable nature of relative paths
+		//Update: This has been re-tested and confirmed to be the case
+		"index" => "main.twig",
 	    //"404" => "404.twig"
     ),
     "pageopts_file" => "config.php",
