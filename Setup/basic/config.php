c7897f789bc405736d3afa34916eda071f23d7af
James Bithell <Jbithell@users.noreply.github.com>
2017-03-29 17:33:49 +0100

    Add error handling into the config file
    This may not seem like the best place to put it - but for transparency I think it defo is

--- Setup/basic/config.php
+++ Setup/basic/config.php
@@ -1,15 +1,23 @@
 <?php
-$CONFIG = Array("NAME" => "This is the name of the project");
+$CONFIG = Array(
+	"NAME" => "This is the name of the project",
+	"DEBUGGING" => false,
+);
 
 $BUNDLER_CONFIG_ARRAY = Array (
-    "files" => Array (
+	"files" => Array (
 		//Hey there maintainer. You may believe it to be a good idea in adding a "users/happy/havefun" page - it works with bundler after all! but you are wrong. Like really very wrong. You couldn't be more wrong in fact. All paths will be broken. Very broken. Such is the lovable nature of relative paths
 		//Update: This has been re-tested and confirmed to be the case
 		"index" => "main.twig",
-	    //"404" => "404.twig"
-    ),
-    "pageopts_file" => "config.php",
-    "static_file_dir" => "/static",
-    "twig_dir" => "/display",
-    "base_url" => ""
+		//"404" => "404.twig"
+	),
+	"pageopts_file" => "config.php",
+	"static_file_dir" => "/static",
+	"twig_dir" => "/display",
+	"base_url" => ""
 );
+
+if ($CONFIG['DEBUGGING']) {
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+}
